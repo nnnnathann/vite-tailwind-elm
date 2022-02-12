01e12b49531243008281188c4e1baba0fcdebbaa
Nathan Bleigh <nathan.bleigh@gmail.com>
2022-02-12 16:31:43 -0600

    linting

--- server/api.ts
+++ server/api.ts
@@ -1,9 +1,4 @@
-import express, {
-  ErrorRequestHandler,
-  NextFunction,
-  Request,
-  Response,
-} from "express";
+import express, { ErrorRequestHandler, Request, Response } from "express";
 import { checkSchema, matchedData, validationResult } from "express-validator";
 
 /**
@@ -11,10 +6,9 @@ import { checkSchema, matchedData, validationResult } from "express-validator";
  */
 export function createServer() {
   let theNumber = 0;
-  let version = 1;
   const app = express();
-  app.get("/theNumber", (req, res, next) => {
-    res.send({ theNumber, version });
+  app.get("/theNumber", (req, res) => {
+    res.send({ theNumber });
   });
 
   const theNumberPost = checkSchema({
@@ -24,23 +18,20 @@ export function createServer() {
       toInt: true,
     },
   });
-  app.post(
-    "/theNumber",
-    theNumberPost,
-    (req: Request, res: Response, next: NextFunction) => {
-      const result = validationResult(req);
-      if (!result.isEmpty()) {
-        return res.status(422).json({ errors: result.array() });
-      }
-      const data = matchedData(req);
-      res.json(data);
+  app.post("/theNumber", theNumberPost, (req: Request, res: Response) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(422).json({ errors: result.array() });
     }
-  );
+    const data = matchedData(req);
+    theNumber = data.theNumber;
+    res.json(data);
+  });
   app.use(errorHandler);
   return app;
 }
 
-const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+const errorHandler: ErrorRequestHandler = (err, req, res) => {
   console.error(JSON.stringify(err.stack));
   const errors =
     process.env.APP_ENV !== "production"
